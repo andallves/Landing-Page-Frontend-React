898df041e5edba27ebdcc88fad2b1e7fd5cb6533
andrealves10a@gmail.com <andrealves10a@gmail.com>
2023-05-31 19:37:50 -0300

    heading component

--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -1,11 +1,18 @@
 import PropTypes from 'prop-types';
 import * as Styled from './styles';
 
-export const Heading = ({ children, light = false }) => {
-  return <Styled.Title light={light}>{children}</Styled.Title>;
+export const Heading = ({ children, colorDark = true, as = 'h1', size = 'large', uppercase = false }) => {
+  return (
+    <Styled.Title colorDark={colorDark} as={as} size={size} uppercase={uppercase}>
+      {children}
+    </Styled.Title>
+  );
 };
 
 Heading.propTypes = {
   children: PropTypes.node.isRequired,
-  light: PropTypes.bool.isRequired,
+  colorDark: PropTypes.bool.isRequired,
+  as: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']).isRequired,
+  size: PropTypes.oneOf(['small', 'medium', 'large', 'huge']).isRequired,
+  uppercase: PropTypes.bool.isRequired,
 };
--- src/components/Heading/stories.jsx
+++ src/components/Heading/stories.jsx
@@ -5,11 +5,9 @@ export default {
   component: Heading,
   args: {
     children: 'The text is dark',
-    light: false,
   },
   argTypes: {
     children: { type: 'string' },
-    light: { type: 'boolean' },
   },
   parameters: {
     background: {
@@ -19,7 +17,7 @@ export default {
 };
 
 export const Light = (args) => <Heading {...args} />;
-export const Dark = (args) => <Heading {...args} light={true} />;
+export const Dark = (args) => <Heading {...args} />;
 
 Light.parameters = {
   backgrounds: {
@@ -29,5 +27,5 @@ Light.parameters = {
 
 Dark.args = {
   children: 'The text is white',
-  light: true,
+  colorDark: false,
 };
