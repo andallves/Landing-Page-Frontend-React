3777e39b9efa28aba2c1c731420d608226326dbe
andrealves10a@gmail.com <andrealves10a@gmail.com>
2023-06-01 12:20:38 -0300

    TextComponent testing

--- src/components/TextComponent/TextComponent.test.jsx
+++ src/components/TextComponent/TextComponent.test.jsx
@@ -1,11 +1,46 @@
 import { screen } from '@testing-library/react';
 import { renderTheme } from '../../styles/render-theme';
+import { theme } from '../../styles/theme';
 import { TextComponent } from '.';
 
 describe('<TextComponent />', () => {
-  it('should render', () => {
+  it('should render the component', () => {
     renderTheme(<TextComponent>Children</TextComponent>);
+    const paragraph = screen.getByText('Children');
 
-    expect(screen.getByRole('heandig')).toBeInTheDocument();
+    expect(paragraph).toBeInTheDocument();
+  });
+
+  it('should render correct font-size', () => {
+    renderTheme(<TextComponent>Children</TextComponent>);
+    const paragraph = screen.getByText('Children');
+
+    expect(paragraph).toHaveStyle({
+      'font-size': theme.fonts.sizes.medium,
+    });
+  });
+
+  it('should render correct tag', () => {
+    const { container } = renderTheme(<TextComponent>Children</TextComponent>);
+    const p = container.querySelector('p');
+
+    expect(p.tagName).toBe('P');
+  });
+
+  it('should match a snapshot', () => {
+    const { container } = renderTheme(<TextComponent>Children</TextComponent>);
+    expect(container).toMatchInlineSnapshot(`
+      .c0 {
+        font-size: 2.4rem;
+      }
+
+      <div>
+        <p
+          class="c0"
+        >
+          Children
+        </p>
+      </div>
+    `);
   });
 });
