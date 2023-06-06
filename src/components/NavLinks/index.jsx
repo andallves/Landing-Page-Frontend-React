e27f0b364d607e5687f2e90785330526de93946b
andrealves10a@gmail.com <andrealves10a@gmail.com>
2023-06-06 17:13:50 -0300

    create a NavLinks component

---
+++ src/components/NavLinks/index.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import * as Styled from './styles';
+import { MenuLink } from '../MenuLink';
+
+export const NavLinks = ({ links = [] }) => {
+  return (
+    <Styled.Container>
+      {links.map((link) => (
+        <MenuLink key={link.link} {...link} />
+      ))}
+    </Styled.Container>
+  );
+};
+
+NavLinks.propTypes = {
+  links: PropTypes.arrayOf(
+    PropTypes.shape({
+      children: PropTypes.string.isRequired,
+      link: PropTypes.string.isRequired,
+      newTab: PropTypes.bool,
+    }),
+  ),
+};
