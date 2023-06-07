5aa8af4a5bbe760602308a50c58bf685dd73b6e2
andrealves10a@gmail.com <andrealves10a@gmail.com>
2023-06-07 09:40:45 -0300

    LogoLink test

--- src/components/LogoLink/LogoLink.test.jsx
+++ src/components/LogoLink/LogoLink.test.jsx
@@ -3,9 +3,31 @@ import { renderTheme } from '../../styles/render-theme';
 import { LogoLink } from '.';
 
 describe('<LogoLink>', () => {
-  it('should render', () => {
-    renderTheme(<LogoLink>Children</LogoLink>);
+  it('should render text logo', () => {
+    renderTheme(<LogoLink link="#target" text="Olá mundo" />);
+    expect(
+      screen.getByRole('heading', { name: 'Olá mundo' }),
+    ).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'Olá mundo' })).toHaveAttribute(
+      'href',
+      '#target',
+    );
+  });
 
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+  it('should render image logo', () => {
+    renderTheme(
+      <LogoLink link="#target" text="Olá mundo" srcImg="image.jpg" />,
+    );
+    expect(screen.getByRole('img', { name: 'Olá mundo' })).toHaveAttribute(
+      'src',
+      'image.jpg',
+    );
+  });
+
+  it('should match a snapshot', () => {
+    const { container } = renderTheme(
+      <LogoLink link="#target" text="Olá mundo" srcImg="image.jpg" />,
+    );
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
