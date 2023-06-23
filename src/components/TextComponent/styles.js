99bd3d7655933dbf4362a618354a83a795b5c697
andallves <andrealves10a@gmail.com>
2023-06-23 20:39:04 -0300

    Change TextComponent media

--- src/components/TextComponent/styles.js
+++ src/components/TextComponent/styles.js
@@ -1,7 +1,15 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.p`
+export const Container = styled.div`
   ${({ theme }) => css`
-    font-size: ${theme.fonts.sizes.medium}
+    font-size: ${theme.fonts.sizes.medium};
+
+    @media ${theme.media.lteSmall} {
+      font-size: ${theme.fonts.sizes.xsmall};
+    }
+
+    @media ${theme.media.lteMedium} {
+      font-size: ${theme.fonts.sizes.small};
+    }
   `}
 `;
