44cbe06c5a9db7b0513907e0cda0f7cfb618e110
andallves <andrealves10a@gmail.com>
2023-06-27 12:07:08 -0300

    Fix GridTwoColumn component styles

--- src/components/GridTwoColumn/index.jsx
+++ src/components/GridTwoColumn/index.jsx
@@ -7,7 +7,7 @@ import { TextComponent } from '../TextComponent';
 export const GridTwoColumn = ({ title, text, srcImg, background = false }) => {
   return (
     <SectionBackground background={background}>
-      <Styled.Container>
+      <Styled.Container background={background}>
         <Styled.TextContainer>
           <Heading uppercase>{title}</Heading>
           <TextComponent>{text}</TextComponent>
--- src/components/GridTwoColumn/styles.js
+++ src/components/GridTwoColumn/styles.js
@@ -1,12 +1,38 @@
-import styled from 'styled-components';
-
+import styled, { css } from 'styled-components';
+import { Title } from '../Heading/styles.js';
 export const Container = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 2fr;
+  ${({ theme, background }) => css`
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    gap: ${theme.spacings.large};
+
+    @media ${theme.media.lteSmall} {
+      grid-template-columns: 1fr;
+      text-align: center;
+    }
+
+    @media ${theme.media.lteMedium} {
+      grid-template-columns: 1fr;
+      text-align: center;
+    }
+
+    ${Title} {
+      margin-bottom: ${theme.spacings.xlarge};
+      color: ${background ? theme.colors.white : theme.colors.primaryColor};
+    }
+  `}
 `;
 
 export const TextContainer = styled.div`
+  ${({ theme }) => css`
+  @media ${theme.media.lteSmall} {
+      margin-bottom: ${theme.spacings.large};
+    }
 
+    @media ${theme.media.lteMedium} {
+      margin-bottom: ${theme.spacings.large};
+    }
+  `}
 `;
 
 export const ImageContainer = styled.div`
