3401f0d15455845acfafd38582f6452208a795e9
andallves <andrealves10a@gmail.com>
2023-06-27 13:44:14 -0300

    Create a GridContent component

---
+++ src/components/GridContent/index.jsx
@@ -0,0 +1,27 @@
+import * as Styled from './styles';
+
+import PropTypes from 'prop-types';
+import { SectionBackground } from '../SectionBackground';
+import { Heading } from '../Heading';
+import { TextComponent } from '../TextComponent';
+
+export const GridContent = ({ title, html, background = false }) => {
+  return (
+    <SectionBackground background={background}>
+      <Styled.Container>
+        <Heading uppercase colorDark={!background}>
+          {title}
+        </Heading>
+        <Styled.Html>
+          <TextComponent>{html}</TextComponent>
+        </Styled.Html>
+      </Styled.Container>
+    </SectionBackground>
+  );
+};
+
+GridContent.propTypes = {
+  title: PropTypes.string.isRequired,
+  html: PropTypes.string.isRequired,
+  background: PropTypes.bool,
+};
