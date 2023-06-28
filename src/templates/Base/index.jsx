74b6004ba64584f37adc9e0f0f504cbb797306a8
andallves <andrealves10a@gmail.com>
2023-06-28 10:34:08 -0300

    Create a Base component to use in the App

---
+++ src/templates/Base/index.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import * as Styled from './styles';
+import { Menu } from '../../components/Menu';
+import { Footer } from '../../components/Footer';
+import { GoTop } from '../../components/GoTop';
+
+export const Base = ({ links = [], logoData, footerHtml, children }) => {
+  return (
+    <>
+      <Menu links={links} logoData={logoData} />
+      <Styled.Container>
+        {children}
+        <Footer html={footerHtml} />
+      </Styled.Container>
+      <GoTop />
+    </>
+  );
+};
+
+Base.propTypes = {
+  ...Menu.propTypes,
+  footerHtml: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,14 +1,9 @@
-import { Heading } from '../../components/Heading';
-import * as Styled from './styles';
+import { Base } from '../Base';
+import { mockBase } from '../Base/stories';
+// import * as Styled from './styles';
 
 function Home() {
-  return (
-    <div className="App">
-      <Styled.Wrapper>
-        <Heading>Texto de título</Heading>
-      </Styled.Wrapper>
-    </div>
-  );
+  return <Base {...mockBase} />;
 }
 
 export default Home;
