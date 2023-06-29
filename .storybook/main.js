4bd6ab46ed744ab86e4d601c9ca1d2f634851da1
andallves <andrealves10a@gmail.com>
2023-06-28 22:48:23 -0300

    New config storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,19 @@
-/** @type { import('@storybook/react-webpack5').StorybookConfig } */
-const config = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)', '../src/**/stories.@(js|jsx|ts|tsx)'],
+module.exports = {
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/**/stories.@(js|jsx|ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/preset-create-react-app',
     '@storybook/addon-interactions',
   ],
-  framework: {
-    name: '@storybook/react-webpack5',
-    options: {},
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-vite',
   },
-  docs: {
-    autodocs: 'tag',
+  features: {
+    storyStoreV7: true,
   },
-  staticDirs: ['..\\public'],
 };
-export default config;
