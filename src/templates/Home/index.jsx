5f82666426c196155701f974be3d795bb10da293
andallves <andrealves10a@gmail.com>
2023-07-03 14:32:34 -0300

    update Home component

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,11 +1,102 @@
-import { Base } from '../Base';
-import { mockBase } from '../Base/mock';
+import { useEffect, useRef, useState } from 'react';
+
 import * as Styled from './styles';
+import { mapData } from '../../api/map-data';
+
+import { GridTwoColumns } from '../../components/GridTwoColumn';
+import { GridContent } from '../../components/GridContent';
+import { GridText } from '../../components/GridText';
+import { GridImage } from '../../components/GridImage';
+
+import { Base } from '../Base';
+import { PageNotFound } from '../PageNotFound';
+import { Loading } from '../Loading';
+import { useLocation } from 'react-router-dom';
 
 function Home() {
+  const [data, setData] = useState([]);
+  const isMounted = useRef(true);
+  const location = useLocation();
+
+  useEffect(() => {
+    const load = async () => {
+      const pathName = location.pathname.replace(/[^a-z0-9-_]/gi, '');
+      const slug = pathName ? pathName : 'my-landing-page';
+      try {
+        const data = await fetch(
+          `http://localhost:1337/api/pages/?filters[slug]=${slug}&populate=deep`,
+        );
+        const json = await data.json();
+        const { attributes } = json.data[0];
+        const pageData = mapData([attributes]);
+        setData(() => pageData[0]);
+      } catch (e) {
+        setData(undefined);
+      }
+    };
+
+    if (isMounted.current === true) {
+      load();
+    }
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, [location]);
+
+  useEffect(() => {
+    if (data === undefined) {
+      document.title = `Página não encontrada | ${config.siteName}`;
+    }
+
+    if (data && !data.slug) {
+      document.title = `Carregando... | ${config.siteName}`;
+    }
+
+    if (data && data.title) {
+      document.title = `${data.title} | ${config.siteName}`;
+    }
+  }, [data]);
+
+  if (data === undefined) {
+    return <PageNotFound />;
+  }
+
+  if (data && !data.slug) {
+    return <Loading />;
+  }
+
+  const { menu, sections, footerHtml, slug } = data;
+  const { links, text, link, srcImg } = menu;
+
   return (
     <Styled.Wrapper>
-      <Base {...mockBase} />;
+      <Base
+        links={links}
+        footerHtml={footerHtml}
+        logoData={{ text, link, srcImg }}
+      >
+        {sections.map((section, index) => {
+          const { component } = section;
+          const key = `${slug}-${index}`;
+
+          if (component === 'section.section-two-columns') {
+            return <GridTwoColumns key={key} {...section} />;
+          }
+
+          if (component === 'section.section-content') {
+            return <GridContent key={key} {...section} />;
+          }
+
+          if (component === 'section.sections-grid-text') {
+            return <GridText key={key} {...section} />;
+          }
+
+          if (component === 'section.sections-grid-image') {
+            return <GridImage key={key} {...section} />;
+          }
+        })}
+      </Base>
     </Styled.Wrapper>
   );
 }
