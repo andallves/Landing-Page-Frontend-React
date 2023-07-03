d4d54690300acec2d2e2e2d0642a1b6beca3afcb
andallves <andrealves10a@gmail.com>
2023-07-03 16:58:20 -0300

    Add routes

--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,17 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './styles/global-styles';
 import { theme } from './styles/theme';
 import Home from './templates/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
       <GlobalStyles />
     </ThemeProvider>
   </React.StrictMode>,
