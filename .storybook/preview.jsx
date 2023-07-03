85aab6283537a14ccc33ff268bc71a77291e0e47
andallves <andrealves10a@gmail.com>
2023-07-03 14:27:03 -0300

    update files

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -4,7 +4,7 @@ import { GlobalStyles } from '../src/styles/global-styles';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
-  controls; {
+  controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   root: 'src',
@@ -15,8 +14,5 @@ export default defineConfig({
     setupFiles: ['../.test/setup.js'],
     include: ['**/*(*.)?{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     exclude: ['node_modules', 'dist', '.idea', '.git', '.cache', 'templates'],
-    // coverage: {
-    //   reporter: ['clover', 'json', 'lcov'],
-    // },
   },
 });
